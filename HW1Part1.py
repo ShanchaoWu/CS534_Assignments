6e1f44317c2afee1c7b99bfd963418ab698b6069
JackHaoyingZhou <zhouhaoying96@outlook.com>
2022-02-04 11:06:43 -0500

    initalize chess board for problem 1

--- HW1Part1.py
+++ HW1Part1.py
@@ -1,14 +1,20 @@
 import numpy as np
+import random
 
 class HeavyQueen:
     def __init__(self, chess_dim=8):
-        self.chess_dim = chess_dim
+        self.n = chess_dim
+        self.chess_board = np.zeros((self.n, self.n))
+
+    def init_borad(self):
+        for i in range(self.n):
+            self.chess_board[random.randint(0, self.n-1), i] = random.randint(1, 9)
 
     def test(self):
-        print(self.chess_dim)
+        print(self.n)
         pass
 
 if __name__ == "__main__":
-    class_a = HeavyQueen(chess_dim=9)
-    class_a.test()
-    print('test')
+    heave_queen = HeavyQueen(chess_dim=8)
+    heave_queen.init_borad()
+    print(heave_queen.chess_board)
